1d5b6683ea55f45ecb5e2018fd1f1bdbb260a35c
Jason Hou <hjj1992@gmail.com>
2016-04-13 13:57:50 +0800

    使用通用消息发送方法

--- lib/mipush/service.rb
+++ lib/mipush/service.rb
@@ -4,9 +4,8 @@ module Mipush
     # 向 reg_id 发送消息
     def self.send_regid(regid, message)
       regid = URI.encode regid.to_s
-      url = SEND_ENDPOINT_REGID + "?" + "registration_id=" + regid + "&" + message.to_params
 
-      RestClient.post url, "", { authorization: "key=#{message.secret_key}" }
+      send_message(SEND_ENDPOINT_REGID, message, {registration_id: regid})
     end
 
     # 向 alias 发送通知
@@ -23,12 +22,12 @@ module Mipush
     #
     # @note 发送推送
     #
-    # @param [String] url 地址
+    # @param [String] endpoint 地址
     # @param [Mipush::Message] message 推送的消息体
     # @param [Hash] params 额外参数
     #
-    def self.send(url, message, params = {})
-      url = "#{url}?#{message.to_params}"
+    def self.send_message(endpoint, message, params = {})
+      url = "#{endpoint}?#{message.to_params}"
       url = "#{url}&#{params.to_param}" if params
       RestClient.post url, '', { authorization: "key=#{message.secret_key}" }
     end
