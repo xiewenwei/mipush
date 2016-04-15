6b5d33a12ce89b5bf24963493ab722f73f7be043
Jason Hou <hjj1992@gmail.com>
2016-04-15 16:39:04 +0800

    添加依赖 rspec , activesupport

--- lib/mipush.rb
+++ lib/mipush.rb
@@ -5,6 +5,8 @@ require "mipush/message"
 require "mipush/ios_message"
 require "mipush/android_message"
 require "mipush/service"
+require 'active_support'
+require 'active_support/core_ext'
 
 module Mipush
   SEND_ENDPOINT_REGID = "https://api.xmpush.xiaomi.com/v2/message/regid"
